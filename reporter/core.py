52050062d1ee815e231d6dbb1c8f4010356bde86
Tim Sutton <tim@linfiniti.com>
2013-02-02 21:07:07 +0700

    Initial implementation of timeline report

--- reporter/core.py
+++ reporter/core.py
@@ -82,11 +82,12 @@ def current_status():
     )
     return render_template('base.html', **context)
 
-@app.route('/user')
-def user_status():
-    username = request.args.get('username')
-    bbox = request.args.get('bbox')
-
+@app.route('/timeline')
+def current_status():
+    mySortedUserList = []
+    bbox = request.args.get('bbox', config.BBOX)
+    tag_name = request.args.get('obj', config.TAG_NAMES[0])
+    error = None
     try:
         coordinates = split_bbox(bbox)
     except ValueError:
@@ -97,6 +98,59 @@ def user_status():
             myFile = get_osm_file(bbox, coordinates)
         except urllib2.URLError:
             error = "Bad request. Maybe the bbox is too big!"
+        else:
+            if not tag_name in config.TAG_NAMES:
+                error = "Unsupported object type"
+            else:
+                mySortedUserList = osm_object_contributions(myFile, tag_name)
+
+
+    myNodeCount, myWayCount = get_totals(mySortedUserList)
+
+    # We need to manually cast float in string, otherwise floats are
+    # truncated, and then rounds in Leaflet result in a wrong bbox
+    # Note: split_bbox should better keep returning real floats
+    coordinates = dict((k, repr(v)) for k, v in coordinates.iteritems())
+
+    context = dict(
+        mySortedUserList=mySortedUserList,
+        myWayCount=myWayCount,
+        myNodeCount=myNodeCount,
+        myUserCount=len(mySortedUserList),
+        bbox=bbox,
+        current_tag_name=tag_name,
+        available_tag_names=config.TAG_NAMES,
+        error=error,
+        coordinates=coordinates,
+        display_update_control=int(config.DISPLAY_UPDATE_CONTROL),
+        )
+    return render_template('timeline.html', **context)
+
+@app.route('/user')
+def user_status():
+    """Get nodes for user as a json doc.
+
+        TODO: What is this used for?
+
+        To use e.g.: http://localhost:5000/user?bbox=20.431909561157227,
+        -34.02849543118406,20.45207977294922,-34.02227106658948&
+        obj=building&username=timlinux
+    """
+    username = request.args.get('username')
+    bbox = request.args.get('bbox')
+
+    try:
+        coordinates = split_bbox(bbox)
+    except ValueError:
+        error = "Invalid bbox"
+        coordinates = split_bbox(config.BBOX)
+        LOGGER.exception(error + coordinates)
+    else:
+        try:
+            myFile = get_osm_file(bbox, coordinates)
+        except urllib2.URLError:
+            error = "Bad request. Maybe the bbox is too big!"
+            LOGGER.exception(error + coordinates)
         else:
             node_data = osm_nodes_by_user(myFile, username)
             return jsonify(d=node_data)
@@ -216,7 +270,7 @@ def osm_object_contributions(theFile, tagName):
         myRecord = {'name': myKey,
                     'ways': myValue,
                     'nodes': myNodeCountDict[myKey],
-                    'timeline': myTimeLines[myKey],
+                    'timeline': interpolated_timeline(myTimeLines[myKey]),
                     'crew': myCrewFlag}
         myUserList.append(myRecord)
 
